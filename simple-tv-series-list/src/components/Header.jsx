7e1ff1a17c97b5d44637f581b42778cbab448db9
mirrerror <cayote20@mail.ru>
2025-05-17 17:21:37 +0300

    remove unused imports

--- simple-tv-series-list/src/components/Header.jsx
+++ simple-tv-series-list/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { logout } from '../services/AuthService';
 import ThemeToggle from './ThemeToggle';
